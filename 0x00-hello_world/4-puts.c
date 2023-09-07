d4138a55ce7f47002920dda9654ce544205eb14a
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-09-07 09:53:09 -0700

    low level langa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
-#include <stdio.h>
+#include <Stdio.h>
+
+/**
+ ** main - Entry point
+ **
+ ** Description: A C program that print with put function
+ **
+ ** Return: Alawys 0 (Success)
+ **/
+
 int main(void)
-/*
-* main
-* return: 0 non-stop
-*/
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
