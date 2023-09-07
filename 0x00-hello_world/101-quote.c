475869c66af82c777344f7ee5f1cf6d1b55a4bc5
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-09-07 16:35:25 -0700

    low level langa

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
 * main - Entry point wizout printf or pust
 * Return: Always 1 (Sucess)
 */
-int void(main)
+int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
