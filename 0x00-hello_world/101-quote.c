829ef46548b7bdef3442f2b53a737a90d41cce59
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-09-07 15:36:11 -0700

    low level langa

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints exactly "and that piece of art is useful" 
+ * - Dora Korpar, 2015-10-19,
+ * return always 0 (success)
+ */
+int void(main)
+{
+	wirte(2,"and that piece of art is useful\" - Dora Korpoar, 2015-10-19\n", 59)
+		return (1);
+}
