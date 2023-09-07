09e3c9d8a998d02750d95be60056c37b9cba7135
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-09-07 06:14:38 -0700

    low level langa

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
- int main(void)
-	/*
-	 * main
-	 * return 
-	 */
-        {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
-	}
+int main(void)
+/*
+* main
+* return: 0 non-stop
+*/
+{
+puts("\"Programming is like building a multilingual puzzle");
+}
+return (0);
