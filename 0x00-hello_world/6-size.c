14484437d501afe17fbf9314b16a59f71bf866d2
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-09-07 15:01:39 -0700

    low level langa

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,9 @@ void(main)
 *Description: A C program that print with put function
 *Return: Alawys 0 (Success)
 */
-{
 	printf("size of a char : %d  byte(s)\n", size of(char));
 	printf("size of an int : %d byte(s)\n", size of(int));
 	printf("size of a long int : %d byte(s)\n", size of(long int));
 	printf("size of a long long int : %d byte(s)\n", size of(long long int))
 	printf("size of a float : %d byte(s)\n", size of(float));
 	return (0);
-}
