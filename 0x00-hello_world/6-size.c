86dfcb2e4cbd4effcf91a07049b7557fd3344702
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-09-07 15:22:33 -0700

    low level langa

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /*
-*main - Entry point
-*Description: A C program that print with put function
+*main - point size of
 *Return: Alawys 0 (Success)
 */
 void(main)
 {
-printf("size of a char : %d  byte(s)\n", size of(char));
-printf("size of an int : %d byte(s)\n", size of(int));
-printf("size of a long int : %d byte(s)\n", size of(long int));
-printf("size of a long long int : %d byte(s)\n", size of(long long int))
-printf("size of a float : %d byte(s)\n", size of(float));
+printf("Size of a char : %d  byte(s)\n", sizeof(char));
+printf("Size of an int : %d byte(s)\n", sizeof(int));
+printf("Size of a long int : %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int : %d byte(s)\n", sizeof(long long int))
+printf("Size of a float : %d byte(s)\n", sizeof(float));
 	return (0);
 }
