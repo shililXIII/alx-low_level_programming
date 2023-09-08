13bf5429db84d031430f3a3dcbbc4985934d2ebe
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-09-08 04:06:31 -0700

    low level var if else while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,15 +12,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if ("n > 0")
 {
-printf("%d is positive\n", n);
+printf("%lu is positive\n", n);
 }
 else if ("n == 0")
 {
-printf("%d is zero\n", n);
+printf("%lu is zero\n", n);
 }
 else
 {
-printf("%d is negative\n", n);
+printf("%lu is negative\n", n);
 }
 return (0);
 }
