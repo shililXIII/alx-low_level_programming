c178fc6d81e442d68fbe5c74c132a969d1294052
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-09-08 04:10:04 -0700

    low level var if else while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
  * main - it's if , if else function
  * betty style doc for function main goes there
@@ -12,15 +13,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if ("n > 0")
 {
-printf("%lu is positive\n", n);
+printf("%d is positive\n", n);
 }
 else if ("n == 0")
 {
-printf("%lu is zero\n", n);
+printf("%d is zero\n", n);
 }
 else
 {
-printf("%lu is negative\n", n);
+printf("%d is negative\n", n);
 }
 return (0);
 }
