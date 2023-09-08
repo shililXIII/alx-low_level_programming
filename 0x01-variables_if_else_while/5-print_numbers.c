5b75432ab764b38f73117765140826472050ede7
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-09-08 12:07:00 -0700

    low level var if else while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print single digit
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print number from 0 to 9
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	for (i = 48; i < 58; i++)
+	{
+		printf("%d", i)
+	}
+	putchar('\n');
+	return (0);
+}
