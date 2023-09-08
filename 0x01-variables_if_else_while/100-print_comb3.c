6e8b77756e2f547eba50d1cda117da72244129fe
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-09-08 12:44:54 -0700

    low level var if else while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - print comb3
+ * Return: always 0 (success)
+*/
+int main(void)
+{
+int n, m;
+for (n = 48; n <= 56; n++)
+{
+for (m = 47; m <= 57; m++)
+{
+if (m > n)
+{
+putchar(n);
+putchar(m);
+if (n != 56 && m != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
+
