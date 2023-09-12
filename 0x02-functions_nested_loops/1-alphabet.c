684eb73aa6a022ba6bd1b42f807023ddf79ba9a4
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-09-11 23:10:53 -0700

    functions nested loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*
+ * print_alphabet - get alpha
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	_putchar(ch);
+	_putchar('\n');
+}
