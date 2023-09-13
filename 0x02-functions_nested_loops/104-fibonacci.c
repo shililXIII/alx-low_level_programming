5aa0dd66d86ed296707826d8795e0a1caad8fca8
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-09-13 08:26:21 -0700

    function nested loop

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -29,17 +29,6 @@ aft1 = (aft / l);
 aft2 = (aft % l);
 printf("%lu", bef);
 
-for (i = 1; i < 91; i++)
-{
-printf(", %lu", aft);
-aft += bef;
-bef = aft - bef;
-
-bef1 = (bef / 1);
-bef2 = (bef % 1);
-aft1 = (bef / 1);
-aft2 = (aft % 1);
-}
 for (i = 92; i < 99; ++i)
 {
 printf(", %lu", aft1 + (aft2 / 1));
