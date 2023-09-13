aba047718d01c9cdf05dee8798051e925773ca6d
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-09-12 18:00:38 -0700

    function nested loop

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-/*
-main-Prints the first 52 fibonacci numbers Return: Nothing! 
+/**
+ * main - Prints the first 52 fibonacci numbers
+ * Return: Nothing!
 */
-int main(void) 
+int main(void)
 {
 	int i = 0;
-	long j= 1, k =2; 
-	while (i < 50)
+	long j = 1, k = 2;
+while (i < 50)
 {
-	if (i == 0) 
-		printf("%ld", j); 
+	if (i == 0)
+		printf("%ld", j);
 	else if (i == 1)
 		printf(", %ld", k);
 	else
@@ -20,6 +21,6 @@ int main(void)
 }
 i++;
 }
-printf("\n");;
+printf("\n");
 return (0);
 }
