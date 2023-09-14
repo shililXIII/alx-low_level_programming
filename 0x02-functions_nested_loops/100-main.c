d6bea6df1d93f10dabbd7dd586de56ba178dde94
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-09-13 19:37:12 -0700

    function nested loop

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -6,12 +6,24 @@
 */ 
 int main(void) 
 { 
-	print_times_table(4); 
-	_putchar('\n'); 
-	print_times_table(6); 
-	_putchar('\n'); 
-	print_times_table(99); 
-	_putchar('\n'); 
-	print_times_table(13); 
+print_times_table(4);
+_putchar('\n');
+print_times_table(6);	
+_putchar('\n');
+print_times_table(99);
+_putchar('\n');
+print_times_table(13);
+_putchar('\n');
+print_times_table(101);
+_putchar('\n');
+print_times_table(1);
+_putchar('\n');
+print_times_table(-3);
+_putchar('\n');
+print_times_table(3);
+_putchar('\n');
+print_times_table(13);
+_putchar('\n');
+print_times_table(13);
 	return (0);
 }
