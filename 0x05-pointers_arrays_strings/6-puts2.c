649a24fecdaea85d71486770b0fb681ade753a1f
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-09-18 15:37:04 -0700

    Pointers arrays and strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * puts2 - only char
+ * @str: input string
+ * Return: char
+*/
+
+void puts2(char *str)
+{
+	int longi = 0;
+
+	int x = 0;
+
+	char *y = str;
+
+	int o;
+
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	x = longi - 1;
+	for (o = 0; o < x; o++)
+	{
+		if (o % 2 == 0)
+		{
+			_putchar(str[o]);
+		}
+	}
+	_putchar('\n');
+}
