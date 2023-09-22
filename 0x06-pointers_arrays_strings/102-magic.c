f89b38a8af4c20ffe076c5902b801aa02539637e
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-09-21 19:44:41 -0700

    more pointer array strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,25 +1,24 @@
-#include "main.h"
-/*
- * main - it's magic try it now
- * Return: always 0.
- */
- int main(void)
-{
-	int n;
-	int a[5];
-	int *p;
-
-	 a[2] = 1024;
-	 p = &n;
-						/*
-						 *  * write ur code here
-						 *   * Remember:
-						 *    * -u can't use a
-						 *     * -u can't mod p
-						 *      * -uR not allowed to code anything
-						 *       */
-						        *(p + 5) = 98;
-							        /* ....it's print 98\n */
-							        printf("a[2] = %d\n", a[2]);
-								return (0);
+#include <stdio.h> 
+  
+ int main(void) 
+	 { 
+		    int n; 
+		       int a[5]; 
+		          int *p; 
+			    
+			     a[2] = 1024; 
+			        p = &n; 
+				   /* 
+				    *     * write your line of code here... 
+				    *         * Remember: 
+				    *             * - you are not allowed to use a 
+				    *                 * - you are not allowed to modify p 
+				    *                     * - only one statement 
+				    *                         * - you are not allowed to code anything else than this line of code 
+				    *                             */ 
+				   ; 
+				      /* ...so that this prints 98\n */ 
+				      printf("a[2] = %d\n", a[2]); 
+				         return (0); 
+					  }
 
