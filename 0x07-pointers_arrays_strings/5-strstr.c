2d95176360fbe3adb1792746b59096c22b19ce14
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-09-25 15:14:27 -0700

    pointers_arrays_strings again

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,8 +11,8 @@ char *_strstr(char *haystack, char *needle)
 {
 	for (; *haystack != '\0'; haystack++)
 	{
-		char *l = *haystack;
-		char *p = *needle;
+		char *l = haystack;
+		char *p = needle;
 
 		while (*l == *p && *p != '\0')
 		{
