f5d0beba49a875853e0e507e4669ac9cffce1d2d
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-10-02 17:35:41 -0700

    argc argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -40,8 +40,8 @@ int main(int argc, char *argv[])
 	count = 1;
 	while (count < argc)
 	{
-		if (check_num(argv[count]))
-		{
+	if (check_num(argv[count]))
+	{
 	str_to_int = atoi(argv[count]);
 	sum += str_to_int;
 	}
