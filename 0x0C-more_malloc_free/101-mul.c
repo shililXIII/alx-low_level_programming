227293183bb73546cc36851f29618458c0130633
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-10-08 07:53:05 -0700

    more malloc free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define ERR_MSG "error"
+#define ERR_MSG "Error"
 
 /**
  * is_digit - check string searching for non-digits
@@ -63,8 +63,8 @@ int main(int argc, char *argv[])
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || is_digit(s1) || is_digit(s2))
-		errors();
+if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		  errors();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
@@ -73,7 +73,7 @@ int main(int argc, char *argv[])
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len--)
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
