388f7d22201801028a95ba6f9cd79ff5553e9280
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-10-11 13:57:54 -0700

    func pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,8 +10,8 @@
 
 int main(int argc, char *argv[])
 {
-	int b, i;
-	char *array;
+	int bytes, i;
+	char *arr;
 
 	if (argc != 2)
 	{
@@ -19,24 +19,24 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 
-	b = atoi(argv[1]);
+	bytes = atoi(argv[1]);
 
-	if (b < 0)
+	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
 
-	array = (char *)main;
+	arr = (char *)main;
 
-	for (i = 0; i < b; i++)
+	for (i = 0; i < bytes; i++)
 	{
-		if (i == b - 1)
+		if (i == bytes - 1)
 		{
-			printf("%02hhx\n", array[i]);
+			printf("%02hhx\n", arr[i]);
 			break;
 		}
-		printf("%02hhx\n", array[i]);
+		printf("%02hhx ", arr[i]);
 	}
 	return (0);
 }
