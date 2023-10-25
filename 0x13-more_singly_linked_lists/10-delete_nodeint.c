497af2649c8dd3e9bf28eb050171a6fdb0253bf8
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-10-24 19:51:08 -0700

    update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,8 +8,8 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	lists_t *tmp = *head;
-	lists_t *curr = NULL;
+	listint_t *tmp = *head;
+	listint_t *curr = NULL;
 	unsigned int i = 0;
 
 	if (*head == NULL)
