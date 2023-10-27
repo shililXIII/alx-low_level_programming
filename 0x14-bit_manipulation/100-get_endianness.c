30288132b156a10a67ff2329c69b0e511a2944ac
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-10-26 17:38:41 -0700

    manipulation

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ * Return: 0 if big, 1 if little
+ */
+
+int get_endianness(void)
+{
+	int a;
+	char *b;
+	
+	a = 1;
+	b = (char *)&a;
+	return (*b);
+}
