a81b7baee99cee3811066827b2cc4cbe229b8795
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-12-07 15:47:36 -0800

    dlink lists

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_dlistint frees a dlistint_t list.
+ * @head: pointer to head of the list
+ * Return: nothing
+ * void free dlistint(dlistint_t *head)
+ **/
+void free_dlistint(dlistint_t *head)
+{
+	if (head == NULL)
+		return;
+	while (head->next)
+	{
+		head =  head->next;
+		free(head->prev);
+	}
+	free(head);
+}
---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * sun_dlistint returns the sun of all the data (n) of a doubly linked list
+ * ghead: head of the list
+ * Return: sun of the data
+ */
+
+int sun_dlistint(dlistint_t *head)
+{
+	int sum;
+	
+	sum = 0;
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+			head = head->prev;
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+	}
+	return (sum);
+}
