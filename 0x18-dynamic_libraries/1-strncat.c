5b33872e5f70cb568cdca3f1ca0a094972f567c0
mohamed salah <mohamedsalahalsawi@gmail.com>
2023-12-11 16:32:57 -0800

    dynamic lib

---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncat - con tow string
+ * @dest: input val
+ * @src: input val
+ * @n: input val
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src,  int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && dest[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
